1606acd892ed397839e1121814a84e3ba3416545
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-01 15:42:16 +0000

    animates sentence to appear one character at a time

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,18 @@
-// hello world
+const sentence = "hello there from lighthouse labs";
+
+// set delay as a variable 
+let x = 1000;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, x);
+  // add 50ms to each each pass to simulae delay
+  x += 50;
+}
+
+x += sentence.length - 1 * 50;
+
+setTimeout(() => {
+  console.log()
+}, x)
